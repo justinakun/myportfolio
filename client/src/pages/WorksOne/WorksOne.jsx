b0ee7a53ce391eaa3963d96a560bc09accdaf54c
Justina Kunigonyte <justina.kunigonyte@gmail.com>
2023-09-27 22:15:39 +0300

    added content to works pages

--- client/src/pages/WorksOne/WorksOne.jsx
+++ client/src/pages/WorksOne/WorksOne.jsx
@@ -1,5 +1,49 @@
+import "./WorksOne.scss";
+
 const WorksOne = () => {
-  return <div>WorksOne</div>;
+  return (
+    <div className="works-one-container">
+      <h1 className="works-one-title">NFT Landing</h1>
+
+      <div className="works-one-description">
+        This is HTML and CSS project.The page is fully responsive with modern
+        design.
+      </div>
+      <div className="works-one-image">
+        <img src="/nft1.png" alt="nft1" />
+      </div>
+      <div className="works-one-image">
+        <img src="/nft2.png" alt="nft2" />
+      </div>
+      <div className="works-one-image">
+        <img src="/nft3.png" alt="nft3" />
+      </div>
+      <div className="works-one-image">
+        <img src="/nft4.png" alt="nft4" />
+      </div>
+      <div className="works-one-image">
+        <img src="/nft5.png" alt="nft5" />
+      </div>
+      <div className="work-one-description">
+        The design is fully responsive:
+      </div>
+      <div className="works-one-small-image">
+        <img src="/nftm1.png" alt="nftm1" />
+      </div>
+      <div className="works-one-small-image">
+        <img src="/nftm2.png" alt="nftm2" />
+      </div>
+      <div className="works-one-small-image">
+        <img src="/nftm3.png" alt="nftm3" />
+      </div>
+      <div className="works-one-small-image">
+        <img src="/nftm4.png" alt="nftm4" />
+      </div>
+      <div className="works-one-small-image">
+        <img src="/nftm5.png" alt="nftm5" />
+      </div>
+    </div>
+  );
 };
 
 export default WorksOne;
--- client/src/pages/WorksTwo/WorksTwo.jsx
+++ client/src/pages/WorksTwo/WorksTwo.jsx
@@ -1,28 +1,52 @@
-import React from "react";
 import "./WorksTwo.scss";
-
+import { Link } from "react-router-dom";
 const WorksTwo = () => {
   return (
     <div className="works-two-container">
       <h1 className="works-two-title">Fun w/ Flags</h1>
       <div className="works-two-description">
         This React app is a simple game, allowing the user to guess countries
-        based on the flags
+        based on the flags. I was inspired by the "Fun with Flags" on the Big
+        Bang Theory. I used React.Js and CSS to create this app. The following
+        image is an example of what a user can see when they open the app:
       </div>
       <div className="works-two-image">
         <img src="/flag1.png" alt="flag1" />
       </div>
-      <div>
-        If the user knows the flag and submits it correctly, they get the
-        following message:
+      <div className="works-two-description">
+        If the user knows the flag and submits the country's name correctly,
+        they are notified about a correct answer:
       </div>
       <div className="works-two-image">
         <img src="/flag4.png" alt="flag4" />
         <img src="/flag5.png" alt="flag5" />
       </div>
-      <div>
-        The user is also given an option to skip by clicking on "Gimme next
-        one!", or learn about the flag by clicking: "Oooops, I don't know"
+      <div className="works-two-description">
+        In case the user clicks on 'submit' button without actually typing
+        anything in, the following message pops up:
+      </div>
+      <div className="works-two-image">
+        <img src="/flag3.png" alt="flag3" />
+      </div>
+      <div className="works-two-description">
+        In the scenario where the user doesn't know the right answer, they are
+        given a possibility to learn. By clicking the 'oops' button, correct
+        answer is revealed:
+      </div>
+      <div className="works-two-image">
+        <img src="/flag6.png" alt="" />
+      </div>
+      <div className="works-two-description">
+        Finally, the user can click on "Gimme next one" button to skip the flag:
+      </div>
+      <div className="works-two-image">
+        <img src="/flag7.png" alt="flag7" />
+      </div>
+      <div className="works-two-description">
+        You can check out the code{" "}
+        <Link to="https://github.com/justinakun/react/tree/main/component_task">
+          here.
+        </Link>
       </div>
     </div>
   );
