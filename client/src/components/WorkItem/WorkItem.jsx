2b366f2ed3362efcd9c0ba8af35914d463e7ea74
Justina Kunigonyte <justina.kunigonyte@gmail.com>
2024-01-22 10:13:38 +0200

    added some responsiveness to works page

--- client/src/components/WorkItem/WorkItem.jsx
+++ client/src/components/WorkItem/WorkItem.jsx
@@ -11,19 +11,21 @@ const WorkItem = ({ number, title, description, skills }) => {
 
   return (
     <div className="work-item">
-      <div className="work-item-number-container">
+      <div className="work-item-first">
         <div className="work-item-number">0{number}</div>
+        <div className="work-item-title">{title}</div>
       </div>
-      <div className="work-item-title">{title}</div>
-      <div className="work-item-description">{description}</div>
-      <div className="work-item-skills">{skills}</div>
-      {route && (
-        <Link to={route.path}>
-          <div className="work-item-more">
-            <MdKeyboardArrowRight className="more-icon" />
-          </div>
-        </Link>
-      )}
+      <div className="work-item-second">{description}</div>
+      <div className="work-item-third">
+        <div className="work-item-skills">{skills}</div>
+        <div className="work-item-button">
+          <Link to={route.path}>
+            <div className="work-item-more">
+              <MdKeyboardArrowRight className="more-icon" />
+            </div>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
