c9faf675c731239755a1596f7445258a450a0226
Justina Kunigonyte <justina.kunigonyte@gmail.com>
2024-02-04 18:18:05 +0200

    eslint warning fixed

--- client/src/components/NotFound/NotFound.jsx
+++ client/src/components/NotFound/NotFound.jsx
@@ -3,13 +3,13 @@ import "./NotFound.scss";
 const NotFound = () => {
   return (
     <div className="not-found-container">
-      <h1>404: Grumpy Cat's Den</h1>
+      <h1>404: Grumpy Cat&apos;s Den</h1>
       <div className="not-found-text">
         <p>
-          Uh-oh! You've landed on the wrong page, and even Grumpy the Cat's not
-          pleased. But don't worry, you can still enjoy a grumpy cat photo below
-          featuring none other than Grumpy himself. It might just make your day
-          better!
+          Uh-oh! You&apos;ve landed on the wrong page, and even Grumpy the
+          Cat&apos;s not pleased. But don&apos;t worry, you can still enjoy a
+          grumpy cat photo below featuring none other than Grumpy himself. It
+          might just make your day better!
         </p>
       </div>
       <img src="/grumpy.jpg" alt="Cat the Grumpy" />
