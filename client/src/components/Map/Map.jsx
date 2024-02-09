d11eeea806c38837fc2ddd895c606897abeaa945
Justina Kunigonyte <justina.kunigonyte@gmail.com>
2024-02-09 21:11:16 +0200

    marker fixed

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -1,6 +1,21 @@
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import PropTypes from "prop-types";
+import L from "leaflet";
 import "./Map.scss";
+
+// marker icons
+import markerIconUrl from "/marker-icon.png";
+import markerShadowUrl from "/marker-shadow.png";
+
+const myMarkerIcon = new L.Icon({
+  iconUrl: markerIconUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowUrl: markerShadowUrl,
+  shadowSize: [41, 41],
+  shadowAnchor: [13, 41],
+});
 
 const Map = ({ coords }) => {
   return (
@@ -11,7 +26,7 @@ const Map = ({ coords }) => {
       className="map"
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={coords}></Marker>
+      <Marker position={coords} icon={myMarkerIcon}></Marker>
     </MapContainer>
   );
 };
